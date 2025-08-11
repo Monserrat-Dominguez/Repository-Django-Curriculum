9480424eed190b77fa91bc45beac56cc7aa47584
Monserrarat Domìnguez <andreamonserratdoare@gmail.com>
2025-08-11 13:39:52 -0600

    Change in settings--DEBUG and ALLOWED_HOSTS

--- curriculum_AMDA/settings.py
+++ curriculum_AMDA/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bko1lys1h)mnj!y_3fftv!0!pg02d@f4$h-durl#y=7$72s1l1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
