083708f33ba0d4c261cf8d920b1ee3ac103a5416
Monserrarat Domìnguez <andreamonserratdoare@gmail.com>
2025-08-12 16:00:57 -0600

    Fix settings

--- curriculumamda/settings.py
+++ curriculumamda/settings.py
@@ -14,10 +14,10 @@ except ImportError:
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-bko1lys1h)mnj!y_3fftv!0!pg02d@f4$h-durl#y=7$72s1l1')
 
 # DEBUG por entorno, por defecto False
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+DEBUG = True 
 
 # Hosts permitidos: desde variable o lista fija
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
