2093c991e9a075944ec9dbdfcfe6620ac39a7dda
Monserrarat Domìnguez <andreamonserratdoare@gmail.com>
2025-08-13 12:24:36 -0600

    Add  in the file views.py the linea from django.http import Http404

--- dates/views.py
+++ dates/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from django.views.generic import DetailView
 from .models import FormacionComplementaria, Historial, Perfil,experiencia
